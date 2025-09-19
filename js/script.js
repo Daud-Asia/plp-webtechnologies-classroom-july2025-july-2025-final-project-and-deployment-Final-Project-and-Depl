b1e40fa97314b312b017cba3908903f424565306
Daud Asia <atinukeasia@gmail.com>
2025-09-19 16:59:36 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
+document.getElementById('signupForm')
+.addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thank you for signing up! We will get back to you soon.');
+  this.reset();
+});
 
+
+function openLogin() {
+  document.getElementById("loginModal").style.display = "block";
+}
+
+function closeLogin() {
+  document.getElementById("loginModal").style.display = "none";
+}
+
+document.getElementById("loginForm")
+.addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Login successful!");
+  closeLogin();
+});
+
+
+
+document.querySelectorAll('.view-btn').forEach(button => {
+  button.addEventListener('click', () => {
+    alert('More details coming soon...');
+  });
+});
+
+document.getElementById('filterBtn').addEventListener('click', () => {
+  alert('Filter functionality will be added soon.');
+});
