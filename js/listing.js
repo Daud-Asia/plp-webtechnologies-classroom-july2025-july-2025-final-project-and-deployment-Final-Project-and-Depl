373d63f4dd0535fea180f0c058603344d7cdd2c5
Daud Asia <atinukeasia@gmail.com>
2025-09-19 16:58:45 +0100

    Create listing.js

---
+++ js/listing.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.view-btn').forEach(button => {
+  button.addEventListener('click', () => {
+    alert('More details coming soon...');
+  });
+});
+
+document.getElementById('filterBtn').addEventListener('click', () => {
+  alert('Filter functionality will be added soon.');
+});
